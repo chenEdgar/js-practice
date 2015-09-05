9af0af1b17e081b0604caea8426355fc67f76a72
Edgar <15989069387@163.com>
2015-09-06 01:06:13 +0800

    经典弹窗效果分解一 --妙味
    解决浏览器对图片的默认事件，直接拖拽元素IE8及以下bug解决；元素resize实现；实现小球碰撞效果

---
+++ luckdog/app.js
@@ -0,0 +1,15 @@
+/**
+ *一个幸运儿的幸运之旅
+ *
+ *快来开启你的幸运之旅吧
+ *
+ * 
+ */
+
+function randomNum (num) {//随机产生1到100的数值
+	return Math.floor(Math.random() * 10) + 1;
+	
+}
+
+console.log(randomNum(num));
+//获取滑块的值
